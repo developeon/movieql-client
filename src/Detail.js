0a4423c01e5f142e2ebc97c4b7fb7eefdbd285fa
soeon Kim <developeon80@gmail.com>
2018-11-12 10:40:27 +0900

    Int cannot represent non-integer value 에러 해결

--- src/Detail.js
+++ src/Detail.js
@@ -7,13 +7,17 @@ const Detail = ({
     params: { movieId }
   }
 }) => (
-  <Query query={MOVIE_DETAILS} variables={{ movieId }}>
+  <Query query={MOVIE_DETAILS} variables={{ movieId: parseInt(movieId) }}>
     {({ loading, error, data }) => {
       if (loading) return <span>loading</span>
       if (error) return <span>something happened</span>
       return (
         <React.Fragment>
-            {data}
+            <p>{data.movie.title}</p>
+            <p>{data.movie.rating}</p>
+            <p>{data.movie.geners}</p>
+            <p>{data.movie.medium_cover_image}</p>
+            <p>{data.movie.description_full}</p>
         </React.Fragment>
       );
     }}
